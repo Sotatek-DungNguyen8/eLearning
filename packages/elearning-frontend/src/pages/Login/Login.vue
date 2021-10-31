6f3a3d0c2c98df1071a018bd1be8b01480fdb1ec
Chi Dung <chidung091@gmail.com>
2021-10-31 16:41:52 +0700

    Update front-end

---
+++ packages/elearning-frontend/src/pages/Login/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    HOME
+    <div class="home-show-status">
+      <p class="rounded-xl p-5 text-red-500 bg-red-400">Hello</p>
+    </div>
+    <router-link to="/hello-world">Say hello</router-link>
+  </div>
+</template>
+
+<script lang="ts" src="./Login.ts"></script>
