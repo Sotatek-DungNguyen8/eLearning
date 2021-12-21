5d1b1da9cab151a66570f18c438db22942c29096
Chi Dung <chidung091@gmail.com>
2021-12-21 22:03:03 +0700

    Fix swagger

--- packages/elearning-backend/src/main.ts
+++ packages/elearning-backend/src/main.ts
@@ -1,21 +1,34 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
-  const config = new DocumentBuilder()
-    .setTitle('eLearning')
-    .setDescription('Trang quản lí api')
-    .setVersion('2.0')
-    .addBearerAuth()
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  setUpSwagger(app);
   app.enableCors()
   await app.listen(3002)
 }
+function setUpSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('eLearning API')
+    .setDescription(
+      `API specification for eLearning.\n
+    Terms:\n
+    - Internal: only available for other services in eLearning system.`,
+    )
+    .setVersion('3.0')
+    .addBearerAuth()
+    .build()
 
+  const document = SwaggerModule.createDocument(app, options)
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      displayOperationId: true,
+    },
+    customSiteTitle: 'eLearning APi',
+  })
+}
 bootstrap()
